f9db95fda977f551e629b0ca70ebe9513db0fa4e
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-03-17 20:09:43 -0300

    first commit

---
+++ back-service-management/src/test/java/com/backservice/backservicemanagement/BackServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backservice.backservicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
